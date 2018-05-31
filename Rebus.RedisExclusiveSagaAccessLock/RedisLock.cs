3827dbdeaef54cc5d7b53a66d6717b8e32bbfceb
Tor Christian <torangel@gmail.com>
2018-05-31 22:08:04 +0200

    Redis based implementation of IExclusiveSagaAccessLock

---
+++ Rebus.RedisExclusiveSagaAccessLock/RedisLock.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Rebus.Sagas.Exclusive;
+using StackExchange.Redis;
+
+namespace Rebus.RedisExclusiveSagaAccessLock
+{
+    public class RedisLock : IExclusiveSagaAccessLock
+    {
+        private readonly ConnectionMultiplexer _multiplexer;
+        private const string DummyValue = "dummy";
+        public RedisLock(ConnectionMultiplexer multiplexer)
+        {
+            _multiplexer = multiplexer ?? throw new ArgumentNullException(nameof(multiplexer));
+        }
+
+        public async Task<bool> AquireLockAsync(string key, CancellationToken cancellationToken)
+        {
+            var db = _multiplexer.GetDatabase();
+            while(!await db.LockTakeAsync(key, DummyValue, TimeSpan.FromMinutes(1),CommandFlags.PreferMaster))
+            {
+                await Task.Delay(20,cancellationToken);
+            }
+            return true;
+        }
+
+        public async Task<bool> ReleaseLockAsync(string key)
+        {
+            var db = _multiplexer.GetDatabase();
+            return await db.LockReleaseAsync(key, DummyValue, CommandFlags.PreferMaster);
+        }
+    }
+}
